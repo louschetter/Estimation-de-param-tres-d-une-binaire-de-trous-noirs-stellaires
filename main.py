99d328c37c089fe185ee4fca71412facad022cbb
Lou Schetter <schetterl@r08.script.univ-paris-diderot.fr>
2026-09-15 17:02:59 +0200

    v150926

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-abc
+import numpy as np
+import numpy.random as rd
+
+def fct1(x, sigma, mu):
+    return np.exp(-((x-mu)**2)/(2*sigma**2))/np.sqrt(2*np.pi*sigma**2)
+
+
+def algo(x0, sigma0, N):
+    tab = [x0]
+    for i in range(1, N+1):
+        x = rd.normal(tab[-1], sigma0)
+
+
+
